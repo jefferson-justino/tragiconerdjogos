03907f85742eccd9734d3f22a8ce2bce38be9153
Jeff <itachisucan@gmail.com>
2024-12-02 17:03:16 -0300

    change games

--- instancia.js
+++ instancia.js
@@ -1,10 +1,10 @@
 let jogo1 = new Guias('https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/208650/header.jpg?t=1702934528',
-                      'Batman arkham knight',13.97,1,
+                      'Batman arkham knight',15.21,1,
                       'https://www.instant-gaming.com/pt/2419-comprar-steam-batman-arkham-knight-pc-jogo-steam/?igr=tragiconerd')
 
 
 let jogo2 = new Guias('https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/203160/header.jpg?t=1710446431',
-                      'Tomb Raider',17.80,1,
+                      'Tomb Raider',19.19,1,
                     'https://www.instant-gaming.com/pt/4401-comprar-steam-tomb-raider-game-of-the-year-edition-goty-edition-pc-mac-jogo-steam/?igr=tragiconerd')
 
 
@@ -14,42 +14,42 @@ let jogo3 = new Guias('https://shared.akamai.steamstatic.com/store_item_assets/s
 
 
 let jogo4 = new Guias('https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1174180/header.jpg?t=1720558643',
-                      'Red dead Redemption 2',102.50,1,
+                      'Red dead Redemption 2',97.76,1,
                     'https://www.instant-gaming.com/pt/5679-comprar-red-dead-redemption-2-pc-jogo-rockstar/?igr=tragiconerd')
 
 
 let jogo5 = new Guias('https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/271590/header.jpg?t=1720472998',
-                      'Grand Theft Auto V',60.70,1,
+                      'Grand Theft Auto V',63.05,1,
                     'https://www.instant-gaming.com/pt/186-comprar-rockstar-grand-theft-auto-v-pc-jogo-rockstar/?igr=tragiconerd')
 
 
 let jogo6 = new Guias('https://gaming-cdn.com/images/products/4044/616x353/disney-epic-mickey-2-the-power-of-two-pc-game-steam-cover.jpg?v=1717600521',
-                      'Disney Epic Mickey 2: The Power of Two',14.46,1,
+                      'Disney Epic Mickey 2: The Power of Two',15.02,1,
                       'https://www.instant-gaming.com/en/4044-buy-disney-epic-mickey-2-the-power-of-two-pc-game-steam/?igr=tragiconerd'
                     )
 
 
 let jogo7 = new Guias('https://gaming-cdn.com/images/products/17024/616x353/god-of-war-ragnarok-pc-game-steam-latin-america-cover.jpg?v=1724665749',
-                      'God of War: Ragnarök',186.40,1,
+                      'God of War: Ragnarök',186.40,0,
                     'https://www.instant-gaming.com/en/17024-buy-god-of-war-ragnarok-pc-game-steam-latin-america/?igr=tragiconerd')
 
 
 let jogo8 = new Guias('https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/2050650/capsule_616x353.jpg?t=1707455805',
-                      'Resident Evil 4 Remake',117.32,0,
+                      'Resident Evil 4 Remake',81.98,1,
                     'https://www.instant-gaming.com/pt/13644-comprar-resident-evil-4-pc-jogo-steam-latin-america/?igr=tragiconerd')
 
 
 let jogo9 = new Guias('https://assets.nuuvem.com/image/upload/t_product_sharing_banner/v1/products/557dbcab69702d0a9ce80501/sharing_images/bdcppcasvjqw45p2h31k.jpg',
-                      'Batman: Arkham City',8.32,0,
+                      'Batman: Arkham City',15.53,1,
                     'https://www.instant-gaming.com/pt/11-comprar-steam-batman-arkham-city-goty-goty-edition-pc-mac-jogo-steam/?igr=tragiconerd')
 
 
 let jogo10 = new Guias('https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/750920/capsule_616x353.jpg?t=1709834464',
-                       'Shadow of the Tomb Raider',40.83,1,
+                       'Shadow of the Tomb Raider',42.03,1,
                     'https://www.instant-gaming.com/pt/5852-comprar-steam-shadow-of-the-tomb-raider-definitive-edition-definitive-edition-pc-mac-jogo-steam/?igr=tragiconerd')
 
 let jogo11 = new Guias('https://gaming-cdn.com/images/products/16474/616x353/ghost-of-tsushima-director-s-cut-pc-jogo-steam-latin-america-cover.jpg?v=1715876591',
-  'Ghost of Tsushima: Directors Cut',182.21,0,
+  'Ghost of Tsushima: Directors Cut',157.71,1,
   'https://www.instant-gaming.com/pt/16474-comprar-ghost-of-tsushima-director-s-cut-pc-jogo-steam-latin-america/?igr=tragiconerd'
 )
 
@@ -59,54 +59,54 @@ let jogo12 = new Guias('https://gaming-cdn.com/images/products/17838/616x353/sil
 )
 
 let jogo13 =new Guias('https://gaming-cdn.com/images/products/17340/616x353/dead-rising-deluxe-remaster-deluxe-remaster-pc-jogo-steam-latin-america-cover.jpg?v=1723125976',
-  'Dead Rising Deluxe Remaster',154.87,1,
+  'Dead Rising Deluxe Remaster',170.02,1,
   'https://www.instant-gaming.com/pt/17340-comprar-dead-rising-deluxe-remaster-deluxe-remaster-pc-jogo-steam-latin-america/?igr=tragiconerd'
 )
 let jogo14 = new Guias('https://gaming-cdn.com/images/products/4836/616x353/marvel-s-avengers-pc-jogo-steam-cover.jpg?v=1704899534',
-'Marvels Avengers',30.99,1,
+'Marvels Avengers',32.19,1,
 'https://www.instant-gaming.com/pt/4836-comprar-steam-marvel-s-avengers-pc-jogo-steam/?igr=tragiconerd'
 )
-let jogo15 = new Guias('https://gaming-cdn.com/images/products/7166/616x353/gotham-knights-pc-game-steam-europe-cover.jpg?v=1706720073','Gotham Knights',19.84,1,
+let jogo15 = new Guias('https://gaming-cdn.com/images/products/7166/616x353/gotham-knights-pc-game-steam-europe-cover.jpg?v=1706720073','Gotham Knights',21.77,1,
   'https://www.instant-gaming.com/en/18033-buy-gotham-knights-pc-game-steam/?igr=tragiconerd'
 )
-let jogo16 = new Guias('https://gaming-cdn.com/images/products/4543/616x353/little-nightmares-complete-edition-complete-edition-pc-game-steam-cover.jpg?v=1710776327','Little Nightmares',20.54,1,
+let jogo16 = new Guias('https://gaming-cdn.com/images/products/4543/616x353/little-nightmares-complete-edition-complete-edition-pc-game-steam-cover.jpg?v=1710776327','Little Nightmares',21.33,1,
   'https://www.instant-gaming.com/en/4543-buy-little-nightmares-complete-edition-complete-edition-pc-game-steam/?igr=tragiconerd'
 )
-let jogo17 =new Guias('https://gaming-cdn.com/images/products/143/616x353/dmc-devil-may-cry-pc-game-steam-europe-cover.jpg?v=1719397004','DMC Devil May Cry',9.72,1,
+let jogo17 =new Guias('https://gaming-cdn.com/images/products/143/616x353/dmc-devil-may-cry-pc-game-steam-europe-cover.jpg?v=1719397004','DMC Devil May Cry',10.10,1,
   'https://www.instant-gaming.com/en/143-buy-dmc-devil-may-cry-pc-game-steam-europe/?igr=tragiconerd'
 )
-let jogo18 = new Guias('https://gaming-cdn.com/images/products/4621/616x353/remnant-from-the-ashes-pc-game-steam-cover.jpg?v=1693467847','Remnant: From the Ashes',40.05,1,
+let jogo18 = new Guias('https://gaming-cdn.com/images/products/4621/616x353/remnant-from-the-ashes-pc-game-steam-cover.jpg?v=1693467847','Remnant: From the Ashes',41.09,1,
   'https://www.instant-gaming.com/en/4621-buy-steam-remnant-from-the-ashes-pc-game-steam/?igr=tragiconerd'
 )
-let jogo19 = new Guias('https://gaming-cdn.com/images/products/8085/616x353/little-nightmares-ii-deluxe-edition-deluxe-edition-pc-game-steam-cover.jpg?v=1712159707','Little Nightmares II',37.24,1,
+let jogo19 = new Guias('https://gaming-cdn.com/images/products/8085/616x353/little-nightmares-ii-deluxe-edition-deluxe-edition-pc-game-steam-cover.jpg?v=1712159707','Little Nightmares II',40.45,1,
   'https://www.instant-gaming.com/en/8085-buy-little-nightmares-ii-deluxe-edition-deluxe-edition-pc-game-steam/?igr=tragiconerd'
 )
-let jogo20 = new Guias('https://gaming-cdn.com/images/products/6728/616x353/batman-arkham-collection-arkham-collection-pc-jogo-steam-cover.jpg?v=1703172296','Batman: Arkham Collection',27.34,1,
+let jogo20 = new Guias('https://gaming-cdn.com/images/products/6728/616x353/batman-arkham-collection-arkham-collection-pc-jogo-steam-cover.jpg?v=1703172296','Batman: Arkham Collection',24.55,1,
   'https://www.instant-gaming.com/pt/6728-comprar-batman-arkham-collection-arkham-collection-pc-jogo-steam/?igr=tragiconerd'
 )
-let jogo21 = new Guias('https://gaming-cdn.com/images/products/18083/616x353/s-t-a-l-k-e-r-2-heart-of-chornobyl-pc-jogo-steam-latin-america-cover.jpg?v=1732276049','S.T.A.L.K.E.R. 2: Heart of Chornobyl',196.10
+let jogo21 = new Guias('https://gaming-cdn.com/images/products/18083/616x353/s-t-a-l-k-e-r-2-heart-of-chornobyl-pc-jogo-steam-latin-america-cover.jpg?v=1732276049','S.T.A.L.K.E.R. 2: Heart of Chornobyl',203.34
   ,1,
   'https://www.instant-gaming.com/pt/18083-comprar-steam-s-t-a-l-k-e-r-2-heart-of-chornobyl-pc-jogo-steam-latin-america/?igr=tragiconerd'
 )
-let jogo22= new Guias('https://gaming-cdn.com/images/products/2457/616x353/devil-may-cry-hd-collection-pc-game-steam-cover.jpg?v=1704905856','Devil May Cry HD Collection',24.12,1,
+let jogo22= new Guias('https://gaming-cdn.com/images/products/2457/616x353/devil-may-cry-hd-collection-pc-game-steam-cover.jpg?v=1704905856','Devil May Cry HD Collection',25.06,1,
   'https://www.instant-gaming.com/en/2457-buy-steam-devil-may-cry-hd-collection-pc-game-steam/?igr=tragiconerd'
 )
-let jogo23 = new Guias('https://gaming-cdn.com/images/products/1525/616x353/injustice-2-pc-game-steam-cover.jpg?v=1705328637','Injustice 2',15.43,1,
+let jogo23 = new Guias('https://gaming-cdn.com/images/products/1525/616x353/injustice-2-pc-game-steam-cover.jpg?v=1705328637','Injustice 2',16.03,1,
   'https://www.instant-gaming.com/en/1525-buy-injustice-2-pc-game-steam/?igr=tragiconerd'
 )
-let jogo24 = new Guias('https://gaming-cdn.com/images/products/1512/616x353/the-elder-scrolls-v-skyrim-special-edition-special-edition-pc-game-steam-cover.jpg?v=1704193818','The Elder Scrolls V: Skyrim',53.28,1,
+let jogo24 = new Guias('https://gaming-cdn.com/images/products/1512/616x353/the-elder-scrolls-v-skyrim-special-edition-special-edition-pc-game-steam-cover.jpg?v=1704193818','The Elder Scrolls V: Skyrim',48.53,1,
   'https://www.instant-gaming.com/en/1512-buy-the-elder-scrolls-v-skyrim-special-edition-special-edition-pc-game-steam/?igr=tragiconerd'
 )
-let jogo25 = new Guias('https://gaming-cdn.com/images/products/13881/616x353/sifu-pc-jogo-steam-cover.jpg?v=1684165162','Sifu',40.16,1,
+let jogo25 = new Guias('https://gaming-cdn.com/images/products/13881/616x353/sifu-pc-jogo-steam-cover.jpg?v=1684165162','Sifu',40.16,0,
   'https://www.instant-gaming.com/pt/13881-comprar-steam-sifu-pc-jogo-steam/?igr=tragiconerd'
 )
-let jogo26 = new Guias('https://gaming-cdn.com/images/products/7001/616x353/detroit-become-human-pc-jogo-steam-cover.jpg?v=1707225882','Detroit: Become Human',63.37,1,
+let jogo26 = new Guias('https://gaming-cdn.com/images/products/7001/616x353/detroit-become-human-pc-jogo-steam-cover.jpg?v=1707225882','Detroit: Become Human',50.43,1,
   'https://www.instant-gaming.com/pt/7001-comprar-steam-detroit-become-human-pc-jogo-steam/?igr=tragiconerd'
 )
 let jogo27 = new Guias('https://gaming-cdn.com/images/products/15957/616x353/tomb-raider-definitive-survivor-trilogy-pc-jogo-steam-latin-america-cover.jpg?v=1721642314','Tomb Raider: Definitive Survivor Trilogy',62.03,0,
   'https://www.instant-gaming.com/pt/15957-comprar-steam-tomb-raider-definitive-survivor-trilogy-pc-jogo-steam-latin-america/?igr=tragiconerd'
 )
-let jogo28 = new Guias('https://gaming-cdn.com/images/products/1640/616x353/rise-of-the-tomb-raider-20-year-celebration-20-year-celebration-pc-mac-jogo-steam-cover.jpg?v=1649865185','Rise of the Tomb Raider',25.64,1,
+let jogo28 = new Guias('https://gaming-cdn.com/images/products/1640/616x353/rise-of-the-tomb-raider-20-year-celebration-20-year-celebration-pc-mac-jogo-steam-cover.jpg?v=1649865185','Rise of the Tomb Raider',26.63,1,
   'https://www.instant-gaming.com/pt/1640-comprar-steam-rise-of-the-tomb-raider-20-year-celebration-20-year-celebration-pc-mac-jogo-steam/?igr=tragiconerd'
 )
 let jogos =[]
